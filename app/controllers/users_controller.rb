66f95960823a0561d56e4d00b45566eb334e4ace
Alex Rincon <alejandro.rincon9@gmail.com>
2016-07-27 21:30:12 -0500

    Add first parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
+	end
+
   def new
 		@user = User.new
   end
 
-	def show
-		@user = User.find(params[:id])
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			# render new user
+		else
+			render 'new'
+		end
 	end
 end
